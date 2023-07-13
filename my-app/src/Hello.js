2dd23ab9a6b82046ede478065815023d95cb45ad
Louis Kwezi <kwezi.louishg@gmail.com>
2023-07-13 04:42:07 +0300

    first commit

---
+++ my-app/src/Hello.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./Hello.css";
+function HelloWorld() {
+  return (
+    <Fragment>
+      <hr />
+      <h1 className="h1-green">Hello, World!</h1>
+    </Fragment>
+  );
+}
+
+export default HelloWorld;
